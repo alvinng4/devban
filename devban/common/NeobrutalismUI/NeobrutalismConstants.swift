5d299fbcf268334c29335ca60e63ca8d3b627bab
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-10-24 13:35:14 +0800

    Add shadowed border view modifiers

---
+++ devban/common/NeobrutalismUI/NeobrutalismConstants.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+/// Contains useful constants for NeobrutalismUI, used as default parameters / internal constants.
+enum NeobrutalismConstants
+{
+    // All Neobrutalism components have border to emphasize the content
+    static let borderColor: Color = .black
+    static let borderWidth: CGFloat = 2
+
+    // All Neobrutalism components have shadows, with a slight offset from the content
+    static let shadowColor: Color = .black
+    static let shadowOffset: CGSize = .init(width: 4, height: 4)
+
+    // Rounded rectangle
+    static let roundedRectangleCornerRadius: CGFloat = 6
+
+    // Animation (e.g. Button click)
+    static let animationDuration: Double = 0.15
+}
