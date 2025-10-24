768afd4672d9097d98f1ecabf3227703948a6b7c
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-10-24 19:28:06 +0800

    Add Neobrutalism button styles

---
+++ devban/common/NeobrutalismUI/ShadowedBorderButton/ShadowedBorderCircleButtonStyle.swift
@@ -0,0 +1,96 @@
+import SwiftUI
+
+/// A button style with a circular shadowed border effect, and an interactive visual effect when pressed. A background
+/// is applied depending on the environment colorScheme.
+///
+/// - Parameters:
+///     - borderColor: Color of the border.
+///     - borderWidth: Width of the border.
+///     - shadowColor: Color of the shadow.
+///     - shadowOffset; x and y offset of the applied shadow effect.
+///     - stayPressed: Whether the button stay pressed visually.
+///
+/// ## Usage
+/// ```swift
+/// Button
+/// {
+///     print("Added!")
+/// }
+/// label:
+/// {
+///     Image(systemName: "plus")
+///         .resizable()
+///         .scaledToFit()
+///         .padding(15)
+///         .frame(width: 50, height: 50)
+/// }
+/// .buttonStyle(ShadowedBorderCircleButtonStyle())
+/// ```
+struct ShadowedBorderCircleButtonStyle: PrimitiveButtonStyle
+{
+    init(
+        borderColor: Color = NeobrutalismConstants.borderColor,
+        borderWidth: CGFloat = NeobrutalismConstants.borderWidth,
+        shadowColor: Color = NeobrutalismConstants.shadowColor,
+        shadowOffset: CGSize = NeobrutalismConstants.shadowOffset,
+        stayPressed: Bool = false,
+    )
+    {
+        self.borderColor = borderColor
+        self.borderWidth = borderWidth
+        self.shadowColor = shadowColor
+        self.shadowOffset = shadowOffset
+        self.stayPressed = stayPressed
+    }
+
+    @Environment(\.colorScheme) private var colorScheme
+
+    private let borderColor: Color
+    private let borderWidth: CGFloat
+    private let shadowColor: Color
+    private let shadowOffset: CGSize
+    private let stayPressed: Bool
+
+    @GestureState private var isPressed = false
+
+    func makeBody(configuration: Configuration) -> some View
+    {
+        /// Whether the button appears pressed visually
+        let visuallyPressed = stayPressed || isPressed
+
+        /// Determine whether the button action gets triggered by the drag distance
+        let drag = DragGesture(minimumDistance: 0)
+            .updating($isPressed)
+            { _, state, _ in
+                state = true
+            }
+            .onEnded
+            { value in
+                let dist = hypot(value.translation.width, value.translation.height)
+                if (dist < NeobrutalismConstants.dragTolerance)
+                {
+                    configuration.trigger()
+                }
+            }
+
+        /// The actual button view
+        return configuration.label
+            .background(colorScheme == .dark ? .darkBackground : .white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(borderColor, lineWidth: borderWidth),
+            )
+            .background(
+                Circle()
+                    .foregroundStyle(shadowColor)
+                    .offset(visuallyPressed ? CGSize(width: 0, height: 0) : shadowOffset),
+            )
+            .offset(visuallyPressed ? shadowOffset : CGSize(width: 0, height: 0))
+            .animation(
+                .easeOut(duration: NeobrutalismConstants.animationDuration),
+                value: visuallyPressed,
+            )
+            .gesture(drag)
+    }
+}
