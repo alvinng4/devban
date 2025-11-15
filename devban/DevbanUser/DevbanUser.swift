df59b9b4f7f60396277ce975db6f6dbad5e427ca
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-11-15 11:34:40 +0800

    [Dev] Creates user file on database when sign up

--- devban/DevbanUser/DevbanUser.swift
+++ devban/DevbanUser/DevbanUser.swift
@@ -1,8 +1,42 @@
+import FirebaseFirestore
+import FirebaseSharedSwift
 import SwiftUI
 
 struct DevbanUser: Codable
 {
     var uid: String
-    var preferredColorScheme: ThemeManager.PreferredColorScheme
-    var theme: ThemeManager.DefaultTheme
+    var preferredColorScheme: ThemeManager.PreferredColorScheme = .auto
+    var theme: ThemeManager.DefaultTheme = .blue
+}
+
+extension DevbanUser
+{
+    private var userCollection: CollectionReference
+    {
+        return Firestore.firestore().collection("users")
+    }
+
+    private var userDocument: DocumentReference
+    {
+        return userCollection.document(self.uid)
+    }
+
+    private var encoder: Firestore.Encoder
+    {
+        let encoder = Firestore.Encoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }
+
+    private var decoder: Firestore.Decoder
+    {
+        let decoder = Firestore.Decoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+
+    func createNewUserOnDatabase() async throws
+    {
+        try userDocument.setData(from: self, merge: false)
+    }
 }
