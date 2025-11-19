79f2986d82d210b33b49c1dc847af2ccd75d14d9
Alvin <154572722+alvinng4@users.noreply.github.com>
2025-11-19 20:30:04 +0800

    [Dev] Add Haptic and Audio managers

---
+++ devban/common/managers/Sound/SoundManager.swift
@@ -0,0 +1,54 @@
+#if targetEnvironment(macCatalyst)
+    import AVFoundation
+
+    final class SoundManager
+    {
+        static let shared = SoundManager()
+
+        private init() {}
+
+        var player: AVAudioPlayer?
+
+        @MainActor
+        func playSuccessSound()
+        {
+            guard DevbanUserContainer.shared.getSoundEffectSetting() else { return }
+
+            guard let url = Bundle.main.url(forResource: "payment_success", withExtension: "caf")
+            else
+            {
+                print("payment_success.caf audio file not found")
+                return
+            }
+
+            do
+            {
+                player = try AVAudioPlayer(contentsOf: url)
+                player?.play()
+            }
+            catch
+            {
+                print("Error playing sound: \(error.localizedDescription)")
+            }
+        }
+    }
+#else
+    import AudioToolbox
+    import CloudStorage
+
+    final class SoundManager
+    {
+        static let shared = SoundManager()
+
+        private init() {}
+
+        @MainActor
+        func playSuccessSound()
+        {
+            guard DevbanUserContainer.shared.getSoundEffectSetting() else { return }
+            guard settingsSoundEffect else { return }
+
+            AudioServicesPlaySystemSound(1407)
+        }
+    }
+#endif
